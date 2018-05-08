13375739f50faef125f24668b22eb1e2fb25b660
Sampo Savolainen <sampo.savolainen@spatineo.com>
2018-05-08 11:07:44 +0300

    Moved FullStackTest to the proper test folder

--- src/test/java/com/spatineo/anonymisator/FullStackTest.java
+++ src/test/java/com/spatineo/anonymisator/FullStackTest.java
@@ -1,4 +1,28 @@
-package com.spatineo.anonymisator.dns;
+package com.spatineo.anonymisator;
+
+/*-
+ * #%L
+ * com.spatineo:log-anonymiser
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2018 Spatineo Inc
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
 
 import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.*;
@@ -17,6 +41,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.spatineo.anonymisator.AnonymiserProcessor;
 import com.spatineo.anonymisator.ApplicationConfiguration;
+import com.spatineo.anonymisator.dns.DnsLookupHandler;
+import com.spatineo.anonymisator.dns.DnsLookupResult;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
