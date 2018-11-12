de63939b880cd257092b618bbe629c298761805a
Sanna Hautala <sanna.hautala@spatineo.com>
2018-11-12 14:10:13 +0200

    Change IPv4 regex and tests fixes #523

--- src/test/java/com/spatineo/anonymisator/AnonymisatorIPv4Test.java
+++ src/test/java/com/spatineo/anonymisator/AnonymisatorIPv4Test.java
@@ -12,12 +12,12 @@ package com.spatineo.anonymisator;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -32,12 +32,12 @@ import org.junit.Test;
 public class AnonymisatorIPv4Test {
 
 	private AnonymiserProcessor anonymisator;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		anonymisator = new AnonymiserProcessor();
 		anonymisator.setIpAddressAnonymiser(new IpAddressAnonymiser() {
-			
+
 			@Override
 			public String processAddressString(String address) {
 				return "--foundit--";
@@ -48,50 +48,50 @@ public class AnonymisatorIPv4Test {
 	@Test
 	public void testOneAddressInMiddle() throws Exception {
 		String foo = anonymisator.process("Hello 194.100.34.1 world");
-		
+
 		assertEquals("Hello --foundit-- world", foo);
 	}
-	
+
 	@Test
 	public void testAddressPartOfAnotherStringShouldNotMatch1() throws Exception {
 		String foo = anonymisator.process("Hello foo194.100.34.1 world");
-		
+
 		assertEquals("Hello foo194.100.34.1 world", foo);
 	}
-	
+
 	@Test
 	public void testAddressPartOfAnotherStringShouldNotMatch2() throws Exception {
 		String foo = anonymisator.process("Hello 194.100.34.1foo world");
-		
+
 		assertEquals("Hello 194.100.34.1foo world", foo);
 	}
-	
+
 	@Test
 	public void testAddressPartOfAnotherStringShouldNotMatch3() throws Exception {
 		String foo = anonymisator.process("Hello bar194.100.34.1foo world");
-		
+
 		assertEquals("Hello bar194.100.34.1foo world", foo);
 	}
 
 	@Test
 	public void testOneAddressAtStart() throws Exception {
 		String foo = anonymisator.process("194.100.34.1 world");
-		
+
 		assertEquals("--foundit-- world", foo);
 	}
-	
+
 
 	@Test
 	public void testOneAddressAtStartWithWhitespace() throws Exception {
 		String foo = anonymisator.process(" 194.100.34.1 world");
-		
+
 		assertEquals(" --foundit-- world", foo);
 	}
 
 	@Test
 	public void testOneAddressAtEnd() throws Exception {
 		String foo = anonymisator.process("Hello 194.100.34.1");
-		
+
 		assertEquals("Hello --foundit--", foo);
 	}
 
@@ -100,7 +100,7 @@ public class AnonymisatorIPv4Test {
 	@Test
 	public void testOneAddressAtEndWithWhitespace() throws Exception {
 		String foo = anonymisator.process("Hello 194.100.34.1 ");
-		
+
 		assertEquals("Hello --foundit-- ", foo);
 	}
 
@@ -109,37 +109,37 @@ public class AnonymisatorIPv4Test {
 	@Test
 	public void testTwoAddressInMiddle() throws Exception {
 		String foo = anonymisator.process("Hello 194.100.34.1 10.1.1.0 world");
-		
+
 		assertEquals("Hello --foundit-- --foundit-- world", foo);
 	}
-	
+
 	@Test
 	public void testTwoAddressOneStartOneMiddle() throws Exception {
 		String foo = anonymisator.process("194.100.34.1 hello 10.1.1.0 world");
-		
+
 		assertEquals("--foundit-- hello --foundit-- world", foo);
 	}
-	
+
 
 	@Test
 	public void testTwoAddressOneMiddleOneEnd() throws Exception {
 		String foo = anonymisator.process("Hello 194.100.34.1 world 10.1.1.0");
-		
+
 		assertEquals("Hello --foundit-- world --foundit--", foo);
 	}
-	
+
 	@Test
 	public void testTwoAddressOneStartOneEnd() throws Exception {
 		String foo = anonymisator.process("194.100.34.1 hello world 10.1.1.0");
-		
+
 		assertEquals("--foundit-- hello world --foundit--", foo);
 	}
-	
+
 
 	@Test
 	public void testPortSeparatedByColon() throws Exception {
 		String foo = anonymisator.process("Hello 194.100.34.1:1234 world");
-		
+
 		// Note: ports vanished becase the IpAddressAnonymiser is supposed to handle it
 		assertEquals("Hello --foundit-- world", foo);
 	}
@@ -148,7 +148,7 @@ public class AnonymisatorIPv4Test {
 	@Test
 	public void testPortSeparatedByColonCommaSeparated() throws Exception {
 		String foo = anonymisator.process("Hello 194.100.34.1:1234,127.0.0.1:32176 world");
-		
+
 		// Note: ports vanished becase the IpAddressAnonymiser is supposed to handle it
 		assertEquals("Hello --foundit--,--foundit-- world", foo);
 	}
@@ -157,127 +157,138 @@ public class AnonymisatorIPv4Test {
 	@Test
 	public void testXForwardedForCommas() throws Exception {
 		String foo = anonymisator.process("Hello 194.100.34.1,255.255.255.255,9.1.2.3 world");
-		
+
 		assertEquals("Hello --foundit--,--foundit--,--foundit-- world", foo);
 	}
 
 	@Test
 	public void testXForwardedForCommasWithSpacesAfterComma() throws Exception {
 		String foo = anonymisator.process("Hello 194.100.34.1, 255.255.255.255, 9.1.2.3 world");
-		
+
 		assertEquals("Hello --foundit--, --foundit--, --foundit-- world", foo);
 	}
 
-	
+
 	@Test
 	public void textWeirdRowWithCommasAndColons() throws Exception {
 		String foo = anonymisator.process("84.192.245.70:59913, 84.192.245.70:59913,127.0.0.1 23.97.211.140 - -");
-		
+
 		// Note: ports vanished becase the IpAddressAnonymiser is supposed to handle it
 		assertEquals("--foundit--, --foundit--,--foundit-- --foundit-- - -", foo);
 	}
-	
-	
+
 	@Test
 	public void testAddressInQuotes() throws Exception {
 		String foo = anonymisator.process("Hello \"127.0.0.1\" world");
 
-		
+
 		assertEquals("Hello \"--foundit--\" world", foo);
 	}
-	
+
 	@Test
 	public void testAddressWithPortInQuotes() throws Exception {
 		String foo = anonymisator.process("Hello \"127.0.0.1:1234\" world");
 
-		
+
 		assertEquals("Hello \"--foundit--\" world", foo);
 	}
-	
+
 
 	@Test
 	public void testAddressWithPortInStartOfQuotes() throws Exception {
 		String foo = anonymisator.process("Hello \"127.0.0.1:1234, stuff\" world");
 
-		
+
 		assertEquals("Hello \"--foundit--, stuff\" world", foo);
 	}
-	
+
 
 	@Test
 	public void testAddressInStartOfQuotes() throws Exception {
 		String foo = anonymisator.process("Hello \"127.0.0.1, stuff\" world");
 
-		
+
 		assertEquals("Hello \"--foundit--, stuff\" world", foo);
 	}
-	
+
 	@Test
 	public void testAddressEndOfQuotes() throws Exception {
 		String foo = anonymisator.process("Hello \"foo, 127.0.0.1\" world");
 
-		
+
 		assertEquals("Hello \"foo, --foundit--\" world", foo);
 	}
-	
+
 	@Test
 	public void testAddressWithPortEndOfQuotes() throws Exception {
 		String foo = anonymisator.process("Hello \"foo, 127.0.0.1:1234\" world");
 
-		
+
 		assertEquals("Hello \"foo, --foundit--\" world", foo);
 	}
-	
-	
+
 
 	@Test
 	public void testAddressInSingleQuotes() throws Exception {
 		String foo = anonymisator.process("Hello '127.0.0.1' world");
 
-		
+
 		assertEquals("Hello '--foundit--' world", foo);
 	}
-	
+
 	@Test
 	public void testAddressWithPortInSingleQuotes() throws Exception {
 		String foo = anonymisator.process("Hello '127.0.0.1:1234' world");
 
-		
+
 		assertEquals("Hello '--foundit--' world", foo);
 	}
-	
+
 
 	@Test
 	public void testAddressWithPortInStartOfSingleQuotes() throws Exception {
 		String foo = anonymisator.process("Hello '127.0.0.1:1234, stuff' world");
 
-		
+
 		assertEquals("Hello '--foundit--, stuff' world", foo);
 	}
-	
+
 
 	@Test
 	public void testAddressInStartOfSingleQuotes() throws Exception {
 		String foo = anonymisator.process("Hello '127.0.0.1, stuff' world");
 
-		
+
 		assertEquals("Hello '--foundit--, stuff' world", foo);
 	}
-	
+
 	@Test
 	public void testAddressEndOfSingleQuotes() throws Exception {
 		String foo = anonymisator.process("Hello 'foo, 127.0.0.1' world");
 
-		
+
 		assertEquals("Hello 'foo, --foundit--' world", foo);
 	}
-	
+
 	@Test
 	public void testAddressWithPortEndOfSingleQuotes() throws Exception {
 		String foo = anonymisator.process("Hello 'foo, 127.0.0.1:1234' world");
 
-		
+
 		assertEquals("Hello 'foo, --foundit--' world", foo);
 	}
-	
+
+	@Test
+	public void testAddressWithAllIPStartEnd0To255() throws Exception {
+
+		for(int i=0; i<256; i++){
+			for(int j=0; j<256; j++) {
+				String foo = anonymisator.process("Hello 'foo, "+i+".0.0."+j+":1234' world");
+
+
+				assertEquals("Hello 'foo, --foundit--' world", foo);
+			}
+		}
+	}
+
 }
--- src/test/java/com/spatineo/anonymisator/FullStackTest.java
+++ src/test/java/com/spatineo/anonymisator/FullStackTest.java
@@ -12,12 +12,12 @@ package com.spatineo.anonymisator;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -51,33 +51,33 @@ public class FullStackTest {
 
 	@Autowired
 	AnonymiserProcessor processor;
-	
+
 	@MockBean
 	DnsLookupHandler dnsLookupHandler;
-	
+
 	@Before
 	public void setup() {
 		processor.setParallelThreads(1);
 	}
-	
+
 	@Test
 	public void testRowWithPortsAndCommas() throws Exception
 	{
 		DnsLookupResult localhostAddress = new DnsLookupResult();
 		localhostAddress.setSuccess(true);
 		localhostAddress.setReverseName("localhost"); // Note: localhost is converted internally into null (as it's a private address)
-		
+
 		when(dnsLookupHandler.lookup("127.0.0.1")).thenReturn(localhostAddress);
 		when(dnsLookupHandler.lookup("192.168.1.72")).thenReturn(localhostAddress);
 		when(dnsLookupHandler.lookup("192.168.1.123")).thenReturn(localhostAddress);
-		
+
 		System.out.println(processor);
 		String str ="127.0.0.1:59913, 192.168.1.72:59913,127.0.0.1 192.168.1.123 - - [01/Feb/2016:21:05:40 +0000] ...";
-		
+
 		StringWriter out = new StringWriter();
 		StringReader in = new StringReader(str);
 		processor.process(in, out);
-		
+
 		String result = out.toString();
 		if (result.endsWith("\n")) {
 			result = result.substring(0, result.length()-1);
@@ -85,6 +85,54 @@ public class FullStackTest {
 		System.out.println(result);
 
 		assertEquals("{!1{127.0.0.0/24}}:59913, {!1{192.168.1.0/24}}:59913,{!1{127.0.0.0/24}} {!1{192.168.1.0/24}} - - [01/Feb/2016:21:05:40 +0000] ...", result);
+	}
+
+	@Test
+	public void testRowWithNumberLooksLikeIPBut4Digits() throws Exception
+	{
+		DnsLookupResult localhostAddress = new DnsLookupResult();
+		localhostAddress.setSuccess(true);
+		localhostAddress.setReverseName("localhost"); // Note: localhost is converted internally into null (as it's a private address)
+
+		System.out.println(processor);
+		String str ="[08/Nov/2018:14:59:34 +0200] Version number SpatialWeb 8.15.3497.0 ...";
+
+		StringWriter out = new StringWriter();
+		StringReader in = new StringReader(str);
+		processor.process(in, out);
+
+		String result = out.toString();
+		if (result.endsWith("\n")) {
+			result = result.substring(0, result.length()-1);
+		}
+		System.out.println(result);
+
+		assertEquals("[08/Nov/2018:14:59:34 +0200] Version number SpatialWeb 8.15.3497.0 ...", result);
+	}
+
+	@Test
+	public void testRowWithNumberLooksLikeIPButNotIP() throws Exception
+	{
+		DnsLookupResult localhostAddress = new DnsLookupResult();
+		localhostAddress.setSuccess(true);
+		localhostAddress.setReverseName("localhost"); // Note: localhost is converted internally into null (as it's a private address)
+
+		when(dnsLookupHandler.lookup("8.15.1.0")).thenReturn(localhostAddress);
+
+		System.out.println(processor);
+		String str ="[08/Nov/2018:14:59:34 +0200] Version number SpatialWeb 8.15.1.0 ...";
+
+		StringWriter out = new StringWriter();
+		StringReader in = new StringReader(str);
+		processor.process(in, out);
+
+		String result = out.toString();
+		if (result.endsWith("\n")) {
+			result = result.substring(0, result.length()-1);
+		}
+		System.out.println(result);
+
+		assertEquals("[08/Nov/2018:14:59:34 +0200] Version number SpatialWeb {!1{8.15.1.0/24}} ...", result);
 	}
 
 }
